0e1dde5c0c601d5e6c45a6ba81a61d0a6d79fd80
Andre <dre4de@gmail.com>
2023-09-01 21:54:51 +0100

    Added a for loop

--- ccode.cpp
+++ ccode.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
-
+void myFunction(){
+    for (int x = 0; x < 6; x++){
+        cout << x << "\n";
+    }
+}
 int main(){
     cout << "Hello world!\n";
+    myFunction();
     return 0;
 }
